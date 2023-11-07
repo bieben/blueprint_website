cc538545d844df3da6dd3826e5fc3a11415f7fbe
phill52 <paner225@gmail.com>
2023-11-07 14:42:28 -0500

    making temporary test pages

---
+++ src/_includes/_layouts/TestPage.jsx
@@ -0,0 +1,9 @@
+import Navbar from '../../_components/Navbar.jsx';
+export default () => (
+  <div>
+    <link href="/styles.css" rel="stylesheet"/>
+    <Navbar />
+    <h1>Hello</h1>
+    {/* The rest of your test page content */}
+  </div>
+); 
---
+++ src/test-page.md
@@ -0,0 +1,8 @@
+---
+layout: _layouts/TestPage.jsx
+title: Test Page
+---
+
+# Test Page
+
+This is a test page to view the Navbar component.
